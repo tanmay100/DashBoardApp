72c0e2ade497595817ce397f72a385b90e3318db
tanmay100 <tanmayyadav100@gmail.com>
2023-02-03 23:20:00 +0530

    Added static file serve code

--- app.js
+++ app.js
@@ -61,8 +61,14 @@ app.use('/api/notifications',notifications)
 //     })
 // }
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname,'public/index.html'))
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname,'public/index.html'))
+// })
+
+app.get('/',(req,res)=>{
+
+    app.use(express.static(path.resolve(__dirname,'public')))
+    res.sendFile(path.resolve(__dirname,'public','index.html'))
 })
 
 
